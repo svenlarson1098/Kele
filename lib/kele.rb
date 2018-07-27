a3d2345caa874909e60a1024006bfd6eabe3c617
svenlarson1098 <svenlarson1098@gmail.com>
2018-07-27 16:58:03 -0700

    Assignment 1 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
 def initialize(email, password)
-  response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: email, password: password} )
+  response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": email, "password": password} )
   raise "Unmatched Authentication: Invalid email/password combination" if response.code != 200
   @auth_token = response["auth_token"]
   end
