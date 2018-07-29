8ca55171ebe075a4a956b46771535b73ece6c518
svenlarson1098 <svenlarson1098@gmail.com>
2018-07-29 13:00:12 -0700

    Assignment 3 Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,7 +12,12 @@ def initialize(email, password)
   end
 
 def get_me
-  response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token})
+  response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token} )
     @user = JSON.parse(response.body)
+  end
+
+def get_mentor_availability(mentor_id)
+  response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token} )
+    @mentor_availability = JSON.parse(response.body)
   end
 end
