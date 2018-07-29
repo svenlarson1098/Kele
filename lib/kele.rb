fd37db45f27ca6ec17f48d16761e9acaf9f96783
svenlarson1098 <svenlarson1098@gmail.com>
2018-07-29 12:35:36 -0700

    Assignment 3 Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -8,5 +9,15 @@ def initialize(email, password)
   response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": email, "password": password} )
   raise "Unmatched Authentication: Invalid email/password combination" if response.code != 200
   @auth_token = response["auth_token"]
+  end
+
+def get_me
+  response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token} )
+    @user = JSON.parse(response.body)
+  end
+
+def get_mentor_availability(mentor_id)
+  response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token} )
+    @mentor_availability = JSON.parse(response.body)
   end
 end
