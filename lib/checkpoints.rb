00869e83eca5b8faed06fdbe97b35364d0c9ab5d
svenlarson1098 <svenlarson1098@gmail.com>
2018-08-07 21:25:16 -0700

    Assignment 6 Complete

--- lib/checkpoints.rb
+++ lib/checkpoints.rb
@@ -2,7 +2,10 @@ module Remaining_Checkpoints
   include HTTParty
 
   def get_remaining_checkpoints(chain_id)
-    response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token } )
-      @remaining_checkpoints = JSON.parse(response.body)
-  end
+      response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token } )
+        @remaining_checkpoints = JSON.parse(response.body)
+    end
 end
+
+
+# irb terminal will only return an empty array due to bug in the Kele endpoint.  
